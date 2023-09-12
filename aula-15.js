4e2217ba20405140c260ef59cb6854d8bdd25435
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-09-11 21:55:19 -0300

    Create aula-15.js

---
+++ aula-15.js
@@ -0,0 +1,92 @@
+// function numeros(num1 ,num2) {
+//     if (num1 === num2){
+//         return"sucesso"
+//     }
+    
+// }
+
+// numeros(Number(prompt("digite um numeros")),(Number(prompt("digite outro numero"))))
+
+// let condicao = false
+
+// if (condicao) {
+//     console.log('entrei no if!')
+// }else{
+//     console.log('entrei no else')
+// }
+
+
+
+// function funcao(num1, num2) {
+//     if (num1 === num2){
+//         return "iguais";
+//     }
+//     else{
+//         return"diferentes";
+//     } 
+// }
+//
+// console.log(funcao(Number(prompt("digite um numeros")),(Number(prompt("digite outro numero")))));
+
+// let condicao1 = false
+// let condicao2 = true
+
+// if (condicao1) {
+//     console.log('entrei no if 1!')
+// }else if(condicao2){
+//         console.log('entrei no if 2!')
+//     }
+
+
+// let condicao3 = false
+// let condicao4 = false
+
+// if (condicao3) {
+//     console.log('entrei no if 1!')
+// }else if(condicao4) {
+//     console.log('netrei no if 2!')
+// }else{
+//     console.log('entrei no else!')
+// }
+
+// function funcao(num1, num2) {
+    
+//     if (num1 === num2){
+//         return "iguais";
+//     }
+//     else if(num1 >= num2){
+//         return "primero numero e maior";
+//     }else{
+//         return "segundo numero e maior";
+//     } 
+// }
+// console.log(funcao(Number(prompt("digite um numeros")),(prompt("digite outro numero"))));
+
+// let paisDeOringem 
+// if (paisDeOringem ==='Brasil'){
+//     console.log('brasileiro')
+// } else if (paisDeOringem === 'EUA'){
+//     console.log('norte americano')}
+//     else if (paisDeOringem ==='Inglaterra'){
+//     console.log('inglês')
+//     }else if (paisDeOringem === 'França'){
+//         console.log('francês')
+//     }
+
+
+// let pokemon = prompt("escolha um pokemon bulbasauro charmander squirtle")
+
+// switch(pokemon){
+//     case 'bulbasauro':
+//         console.log('tipo: planta e veneno')
+//         break
+//     case 'charmander':
+//         console.log('tipo: fogo')
+//         break
+//     case 'squirtle':
+//         console.log('tipo: água')
+//         break
+//     default:
+//         console.log ("pokemon não cadastrado")
+//         break
+// } 
